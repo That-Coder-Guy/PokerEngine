af28a8b43bc1e85a5b931914f5787f8e350dc137
Henry Glenn <hgglenn518@gmail.com>
2024-09-07 22:00:55 -0500

    Added some deck logic

--- PokerSolver/Deck/Deck.cs
+++ PokerSolver/Deck/Deck.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
@@ -9,31 +10,94 @@ namespace PokerSolver.Deck
 {
     public class Deck
     {
-        private List<Card?> _dealtCards = new();
+        private const int SuitCount = 4;
 
-        public static uint TotalCards = 52;
+        private const int RankCount = 13;
 
-        public uint CardsInDeck { get; set; } = TotalCards;
+        private List<Card?> _knownDealtCards = new();
 
-        public uint CardsOutOfDeck => TotalCards - CardsInDeck;
+        public const int TotalCards = 52;
+
+        public int CardsInDeck { get; private set; } = TotalCards;
+
+        public int CardsOutOfDeck => TotalCards - CardsInDeck;
 
         public void DrawRandom()
         {
-            CardsInDeck--;
-            _dealtCards.Add(null);
+            if (CardsInDeck > 0)
+            {
+                CardsInDeck--;
+                //_dealtCards.Add(null);
+            }
+            throw new InvalidOperationException("No cards left in deck.");
+        }
+
+        public void DrawCard(Card card)
+        {
+            if (CardsInDeck > 0)
+            {
+                CardsInDeck--;
+                _knownDealtCards.Add(card);
+            }
+            throw new InvalidOperationException("No cards left in deck.");
         }
 
         public double DrawProbability(Card card)
         {
-            if (_dealtCards.Contains(card))
+            if (_knownDealtCards.Contains(card))
+            {
+                return 0;
+            }
+            else if (CardsInDeck == 0)
             {
                 return 0;
             }
             else
             {
-                double chanceDealt = _dealtCards.Count(card => card == null) / TotalCards;
-                double chanceNotDealt = (CardsInDeck + _dealtCards.Count(card => card != null)) / TotalCards;
+                double chanceToDraw = 1.0 / CardsInDeck;
+                int unknownCardCount = CardsOutOfDeck - _knownDealtCards.Count();
+
+                if (unknownCardCount == 0)
+                {
+                    return chanceToDraw;
+                }
+                else
+                {
+                    double chanceDealt = unknownCardCount / TotalCards;
+                    double chanceNotDealt = (CardsInDeck + _knownDealtCards.Count()) / TotalCards;
+                    return chanceNotDealt * chanceToDraw;
+                }
             }
         }
+        public double DrawProbability(Rank rank)
+        {
+            Card?[] dealtCardsWithRank = _knownDealtCards.Where(card => card != null).ToArray();
+
+            if (_knownDealtCards.Contains(card))
+            {
+                return 0;
+            }
+            else if (CardsInDeck == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                double chanceToDraw = 1.0 / CardsInDeck;
+                int unknownCardCount = _knownDealtCards.Count(card => card == null);
+
+                if (unknownCardCount == 0)
+                {
+                    return chanceToDraw;
+                }
+                else
+                {
+                    double chanceDealt = unknownCardCount / TotalCards;
+                    double chanceNotDealt = (CardsInDeck + _knownDealtCards.Count(card => card != null)) / TotalCards;
+                    return chanceNotDealt * chanceToDraw;
+                }
+            }
+        }
+
     }
 }
