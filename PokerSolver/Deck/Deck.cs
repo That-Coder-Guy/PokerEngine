c9c5323ae5aebfe24cee7dfe4b2922ccda440dd7
hglenn2 <hglenn2@ENGG-E1113-25>
2024-09-07 19:01:43 -0500

    Add project files.

---
+++ PokerSolver/Deck/Deck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PokerSolver.Deck
+{
+    public class Deck
+    {
+        private List<Card?> _dealtCards = new();
+
+        public static uint TotalCards = 52;
+
+        public uint CardsInDeck { get; set; } = TotalCards;
+
+        public uint CardsOutOfDeck => TotalCards - CardsInDeck;
+
+        public void DrawRandom()
+        {
+            CardsInDeck--;
+            _dealtCards.Add(null);
+        }
+
+        public double DrawProbability(Card card)
+        {
+            if (_dealtCards.Contains(card))
+            {
+                return 0;
+            }
+            else
+            {
+                double chanceDealt = _dealtCards.Count(card => card == null) / TotalCards;
+                double chanceNotDealt = (CardsInDeck + _dealtCards.Count(card => card != null)) / TotalCards;
+            }
+        }
+    }
+}
---
+++ PokerSolver/Deck/Rank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerSolver.Deck
+{
+    public enum Rank
+    {
+        Ace = 1,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
